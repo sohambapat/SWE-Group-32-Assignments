087b7d6d343a7e7b8d171d14d007373cca666080
Seoyeong Park <spark43@ncsu.edu>
2022-09-01 21:40:58 -0400

    sym constructor and mode function

---
+++ src/Sym.py
@@ -0,0 +1,24 @@
+class Sym:
+    # Constructor Sym:new(c,s) 
+    def __init__(self,c,s):
+        self.n = 0
+        # self.at = c if self.at else 0
+        if c != 0:
+            self.at = c
+        else:
+            self.at = 0
+        # self.name = s if self.name else ""
+        if s != "":
+            self.name = s
+        else:
+            self.name = ""
+        # _has = {}
+        self._has = {}
+
+
+    # function Sym:mid(col,    most,mode) 
+    def mid(self, col, mode, most=-1):
+        for k, v in self._has.items():
+            if v > most :
+                mode, most = k, v
+        return mode
