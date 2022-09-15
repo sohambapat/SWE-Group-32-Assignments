66a5b376b797d04572b65bb5aa199a44f05e4fd3
cmriggs <cmriggs@ncsu.edu>
2022-09-15 14:11:28 -0400

    Removed unused imports and added new ones Changed references of "type()" to "isinstance" since type was not working properly. Added reliance on Util methods (csv) Added stats method (untested)

--- src/Data.py
+++ src/Data.py
@@ -1,15 +1,16 @@
-import re
-import string # Regex library, allowed by Q36 on discord
-from Num import Num
-from Sym import Sym
 from Cols import Cols
+from Row import Row
+import Utils
+from math import floor
 
 class Data:
     def __init__(self, src):
         self.cols = None
         self.rows = []
-        if type(src) == string:
-            csv(src, self.add())
+        if isinstance(src, str):
+            def function(row):
+                self.add(row)
+            Utils.csv(src, function)
         else:
             for _, row in src:
                 self.add(row)
@@ -22,8 +23,32 @@ class Data:
         if not self.cols:
             self.cols = Cols(xs)
         else:
-            row.append(self.rows)
-            for _, todo in [(i, j) for i in self.cols.x for j in self.cols.y if i != j]:
-                for _, col in todo.items():
-                    Cols.add(row.cells[index(col)])
-        
+            if isinstance(xs, Row):
+                row = xs
+                self.rows.append(xs)
+            else:
+                row = Row(xs)
+                self.rows.append(row)
+            for _, col in enumerate(self.cols.x):
+                col.add(row.cells[col.at])
+            for _, col in enumerate(self.cols.y):
+                col.add(row.cells[col.at])
+
+    def stats(self, places, showCols, fun):
+        if showCols is None:
+            showCols = self.cols.y
+        if fun is None:
+            fun = "mid"
+        t = {}
+        for _,col in showCols.items():
+            v = fun(col)
+            if type(v) == (int or float):
+                if places is not None:
+                    mult = 10**places
+                else:
+                    mult = 10**2
+                v = floor(v * mult + 0.5) / mult
+            else:
+                v = v
+            t[col.name] = v
+        return t
