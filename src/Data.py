bba2f3c7afa0887aa9ad93aad74352772e79cf4e
cmriggs <cmriggs@ncsu.edu>
2022-09-19 18:09:36 -0400

    Documented Data.py

--- src/Data.py
+++ src/Data.py
@@ -5,6 +5,12 @@ from math import floor
 
 class Data:
     def __init__(self, src):
+        """Initializes the data from the csv. Responsible for 
+        opening the csv and storing all of the data. Keeps track 
+        of the column headers and the row's information.
+        
+        Parameters:
+        src (str): The name of the csv file"""
         self.cols = None
         self.rows = []
         if isinstance(src, str):
@@ -19,6 +25,12 @@ class Data:
 
     # function Data:add()
     def add(self, xs):
+        """Adds a row from the csv to the data. If it is the first
+        row of the csv set the row to be the headers (cols).
+        
+        Parameters:
+        xs (Row): The Row object being added to the data
+        xs (str): The string to be converted to a row object and added to the data"""
         row = []
         if not self.cols:
             self.cols = Cols(xs)
@@ -35,6 +47,12 @@ class Data:
                 col.add(row.cells[col.at])
 
     def stats(self, places, showCols, fun):
+        """Takes stats from the columns of data and returns them
+        
+        Parameter:
+        places (int): The amount of decimal places (default 2)
+        showCols (list): A list of all the columns
+        fun (function): A function that gathers the statistic from the data in the column"""
         if showCols is None:
             showCols = self.cols.y
         if fun is None:
